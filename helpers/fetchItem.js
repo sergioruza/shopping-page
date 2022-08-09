21899168d1aede6f3395cc2c0f23d8935255fc0f
Sergio <sergioruza22@gmail.com>
2022-08-09 18:42:53 -0300

    Requisito três, ok

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,14 @@
-const fetchItem = async () => {
+const fetchItem = async (param) => {
+  try {
+    const url = `https://api.mercadolibre.com/items/${param}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    return new Error('You must provide an url');
+  }
 };
+fetchItem('MLB1615760527');
 
 if (typeof module !== 'undefined') {
   module.exports = {
